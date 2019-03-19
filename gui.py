cbb4d8dc29e0e102bd1981d702fad8f86a7b387a
agent515 <46898603+agent515@users.noreply.github.com>
2019-03-19 23:17:34 +0530

    Add files via upload
    Uploaded gui.py file
    Raw code is entered!

---
+++ gui.py
@@ -0,0 +1,42 @@
+from tkinter import Tk, Label, Button
+import image_editor as ie
+
+class MyFirstGUI:
+    def __init__(self, master):
+        self.master = master
+        master.title("UTOPIA IE")
+
+        self.label = Label(master, text="WELCOME TO THE UTOPIA IMAGE EDITOR")
+        self.label.pack()
+
+        self.open_button = Button(master, text="Open", command= self.open())
+        self.open_button.pack()
+
+        self.rotate_button = Button(master, text="Rotate",command= master.rotate(self.img))
+        self.rotate_button.pack()
+
+        self.resize_button = Button(master, text="Resize",command= ie.resize(self.img))
+        self.resize_button.pack()
+
+        self.mirror_button = Button(master, text="Mirror",command= ie.mirror(self.img))
+        self.mirror_button.pack()
+
+        self.ioi_button = Button(master, text="Img over Img",command= self.pop())
+        self.ioi_button.pack()
+
+        self.close_button = Button(master, text="Close", command=master.quit)
+        self.close_button.pack()
+
+    def open(self):
+        print("Enter file path:")
+        self.img = input()
+        ie.properties(self.img)
+
+    def pop(self):
+        print("Enter file path of the other image:")
+        self.img2 = input()
+        ie.picture_over_picture(self.img,self.img2)
+
+root = Tk()
+my_gui = MyFirstGUI(root)
+root.mainloop()
